4aafce9f7c79cf058a8232b21e75b5bfc282bee0
Leslie Xitumul <lxitumulp@gmail.com>
2026-09-22 15:14:04 -0600

    hice in cambio

--- hola.py
+++ hola.py
@@ -1,7 +1,8 @@
-print("hola mundo")
-print ("pro less do")
 print ("dia nacional")
 print("flores amarillas")
 print("ya tu sabes")
 print("morir o vivir")  
 print("no me importa")
+
+x = 5+5
+print(x)
